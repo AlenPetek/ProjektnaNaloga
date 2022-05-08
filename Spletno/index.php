cf98ab6fffc73328ec3f2f40760a221a0d8ea0e9
AlenPetek <100556286+AlenPetek@users.noreply.github.com>
2022-05-08 18:50:12 +0200

    Add files via upload

---
+++ Spletno/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projektna</title>
+		<link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="topnav">
+			<a href="login.php" style="float: right">Register</a>
+			<div class="login-container">
+			<form action="/action_page.php">
+				<input type="text" placeholder="Username" name="username">
+				<input type="text" placeholder="Password" name="psw">
+				<button type="submit">Login</button>
+			</form>
+		  </div>
+		</div>
+		
+		<div id="zemljevid" width="100%" height="2000">
+			<p>Zemljevid</p>
+		</div>
+		<div id="zemljevid" width="100%" height="2000">
+			<p>Tabela živali</p>
+		</div>
+    </body>
+</html> 
